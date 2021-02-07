975b1addf3221b8cba2c90ee568c76d561a2da3b
Ashish Ghosh <Ashish.Ghosh2@ing.com>
2021-02-07 16:16:03 +0100

    Test 03

--- spec/test.account.js
+++ spec/test.account.js
@@ -9,13 +9,13 @@ fixture `Login and Registration` .page `http://awswrkshpalb-1570520390.us-west-2
        // const loginError=(LoginPage.loginerror).exists();
        // await t.expect(loginError).ok();
     });
-    // test('Account Creation should be successful', async t => {
-    //     let random = Math.floor(Math.random()*90000) + 10000;
-    //     let email = accountData.create.email.replace('Ashish','Ashish'+random); 
-    //     await LoginPage.createAccount(accountData.create.firstname,accountData.create.lastname,email,accountData.create.password)
-    //     const accountCreation=(SearchPage.searchfield).exists();
-    //     await t.expect(accountCreation).ok();
-    // });
+    test('Account Creation should be successful', async t => {
+         let random = Math.floor(Math.random()*90000) + 10000;
+         let email = accountData.create.email.replace('Ashish','Ashish'+random); 
+         await LoginPage.createAccount(accountData.create.firstname,accountData.create.lastname,email,accountData.create.password)
+       //  const accountCreation=(SearchPage.searchfield).exists();
+       //  await t.expect(accountCreation).ok();
+     });
 
 
 
