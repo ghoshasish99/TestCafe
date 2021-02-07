55de04c09b759125b34ad272ebc589911f9ff6bd
Ashish Ghosh <Ashish.Ghosh2@ing.com>
2021-02-07 16:20:06 +0100

    Test 04

--- spec/test.account.js
+++ spec/test.account.js
@@ -7,14 +7,14 @@ fixture `Login and Registration` .page `http://awswrkshpalb-1570520390.us-west-2
     test('login should fail with invalid credentials', async t => {
         await LoginPage.login(accountData.negativeLogin.username,accountData.negativeLogin.password);
        // const loginError=(LoginPage.loginerror).exists();
-       // await t.expect(loginError).ok();
+        await t.expect(await (LoginPage.loginerror).exists).ok();
     });
     test('Account Creation should be successful', async t => {
          let random = Math.floor(Math.random()*90000) + 10000;
          let email = accountData.create.email.replace('Ashish','Ashish'+random); 
          await LoginPage.createAccount(accountData.create.firstname,accountData.create.lastname,email,accountData.create.password)
-       //  const accountCreation=(SearchPage.searchfield).exists();
-       //  await t.expect(accountCreation).ok();
+         const accountCreation=(SearchPage.searchfield).exists;
+         await t.expect(accountCreation).ok();
      });
 
 
